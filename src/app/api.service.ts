93daff8cfef0609301d4ff34f3b0cd8ec56b7fd2
Riya505 <riyapk505@gmail.com>
2022-10-14 03:27:42 +0530

    new

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -11,4 +11,7 @@ export class ApiService {
   viewData=()=>{
     return this.http.get("http://dummyapifriends.herokuapp.com/view")
   }
+  addData=(data:any)=>{
+    return this.http.post("https://dummyapifriends.herokuapp.com/adddata",data)
+  }
 }
